c7dfed6d630b98443f911022a5453a429895471f
arpitkeer <85611581+arpitkeer@users.noreply.github.com>
2022-02-01 08:37:16 +0530

    Update header.py

--- header.py
+++ header.py
@@ -16,9 +16,61 @@ y=len(lines)
 for i in range(0,y):
     x=lines[i]
     url= "https://"+x
-    headers={'X-Forwarded-Host': 'exploit-ac.com', 
-    'X-Forwarded-For': 'exploit-ac.com', 
-    'X-Host': 'exploit-ac.com'}
+    headers={'X-Forwarded-Host': 'exploit-ac.com'}
+    try:
+        r = requests.get(url,timeout=10, verify=False)
+        r.raise_for_status()
+    except requests.exceptions.RequestException as err:
+    	print ("OOps: Something Else",err)
+    except requests.exceptions.HTTPError as errh:
+    	print ("Http Error:",errh)
+    except requests.exceptions.ConnectionError as errc:
+    	print ("Error Connecting:",errc)
+    except requests.exceptions.Timeout as errt:
+    	print ("Timeout Error:",errt)     
+    	y=r.status_code
+    	if y==200:
+ 	   	    z=r.text
+ 	   	    b=(re.search('exploit-ac.com', z, re.IGNORECASE))
+ 	   	    if b:
+ 	   	    	print("nice")
+ 	   	    	print(url)
+ 	   	    	hack += [url]
+ 	   	    else:
+ 	   	    	print("sorry")
+    	else:
+        	print("status error", y)
+for i in range(0,y):
+    x=lines[i]
+    url= "https://"+x
+    headers={'X-Forwarded-For': 'exploit-ac.com'}
+    try:
+        r = requests.get(url,timeout=10, verify=False)
+        r.raise_for_status()
+    except requests.exceptions.RequestException as err:
+    	print ("OOps: Something Else",err)
+    except requests.exceptions.HTTPError as errh:
+    	print ("Http Error:",errh)
+    except requests.exceptions.ConnectionError as errc:
+    	print ("Error Connecting:",errc)
+    except requests.exceptions.Timeout as errt:
+    	print ("Timeout Error:",errt)     
+    	y=r.status_code
+    	if y==200:
+ 	   	    z=r.text
+ 	   	    b=(re.search('exploit-ac.com', z, re.IGNORECASE))
+ 	   	    if b:
+ 	   	    	print("nice")
+ 	   	    	print(url)
+ 	   	    	hack += [url]
+ 	   	    else:
+ 	   	    	print("sorry")
+    	else:
+        	print("status error", y)
+for i in range(0,y):
+    x=lines[i]
+    url= "https://"+x
+    headers={'X-Host': 'exploit-ac.com'}
     try:
         r = requests.get(url,timeout=10, verify=False)
         r.raise_for_status()
